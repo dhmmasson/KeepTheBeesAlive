ec6a03467793525dd834abbfb5659e926d28072c
dhmmasson <dhm.masson+github@gmail.com>
2020-04-18 22:05:22 +0200

    Basic level set up-
    Generate a bunch of random objects that are dropped on the floor

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+    // Reference to the Prefab. Drag a Prefab into this field in the Inspector.
+    public int width = 10;
+    public int height = 4;
+    public buildingBlocks level ;
+    void Start()
+    {
+      foreach (buildingBlocks.elementSettings element in level.elements) {
+        for( int i = 0 ; i < element.repetition ; i++ ){
+          float x = Random.Range(-2, 2) ;
+          float y = Random.Range(1, 4) ;
+          Instantiate(element.asset, new Vector3(x,y,0), Quaternion.identity);
+        }
+      }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
---
+++ Assets/Scripts/buildingBlocks.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class buildingBlocks : ScriptableObject
+{
+  public elementSettings[] elements ;
+
+  [System.Serializable]
+  public class elementSettings {
+    public GameObject asset ;
+    [Range(0,20)]
+    public int repetition = 1 ;
+    public Vector3 dropCenter = new Vector3(0,2,0);
+    public int spreadRadius = 0 ;
+  }
+
+}
